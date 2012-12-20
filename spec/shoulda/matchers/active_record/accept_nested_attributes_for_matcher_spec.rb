449564a02209f5cb867f070c907056ef6cf5d7b7
Gabe Berke-Williams <gabe@thoughtbot.com>
2012-12-20 00:04:27 -0500

    Begin the long process of using current thoughtbot style

--- spec/shoulda/matchers/active_record/accept_nested_attributes_for_matcher_spec.rb
+++ spec/shoulda/matchers/active_record/accept_nested_attributes_for_matcher_spec.rb
@@ -1,44 +1,39 @@
-require 'spec_helper'
+require "spec_helper"
 
 describe Shoulda::Matchers::ActiveRecord::AcceptNestedAttributesForMatcher do
-  before do
-    define_model :child, :parent_id => :integer
-    define_model :parent do
-      has_many :children
-    end
+  it "accepts an existing declaration" do
+    accepting_children.should accept_nested_attributes_for(:children)
   end
 
-  let(:parent) { Parent.new }
-  let(:matcher) { accept_nested_attributes_for(:children) }
+  it "rejects a missing declaration" do
+    matcher = accepts_nested_attributes_for(:children)
 
-  it "should accept an existing declaration" do
-    Parent.accepts_nested_attributes_for :children
-    matcher.matches?(parent).should be_true
-  end
+    matcher.matches?(accepting_children).should be_false
 
-  it "should reject a missing declaration" do
-    matcher.matches?(parent).should be_false
     matcher.failure_message.should == "Expected Parent to accept nested attributes for children (is not declared)"
   end
 
   context "allow_destroy" do
-    it "should accept a valid truthy value" do
-      Parent.accepts_nested_attributes_for :children, :allow_destroy => true
-      matcher.allow_destroy(true).matches?(parent).should be_true
+    it "accepts a valid truthy value" do
+      matching = accepting_children(:allow_destroy => true)
+
+      accept_nested_attributes_for.allow_destroy(true).matches?(matching).should be_true
     end
 
-    it "should accept a valid falsey value" do
-      Parent.accepts_nested_attributes_for :children, :allow_destroy => false
-      matcher.allow_destroy(false).matches?(parent).should be_true
+    it "accepts a valid falsey value" do
+      matching = accepting_children(:allow_destroy => false)
+
+      matching.should accept_nested_attributes_for(:children).allow_destroy(false)
     end
 
-    it "should reject an invalid truthy value" do
+    it "rejects an invalid truthy value" do
+      fail "FIXME, start here"
       Parent.accepts_nested_attributes_for :children, :allow_destroy => true
       matcher.allow_destroy(false).matches?(parent).should be_false
       matcher.failure_message.should =~ /should not allow destroy/
     end
 
-    it "should reject an invalid falsey value" do
+    it "rejects an invalid falsey value" do
       Parent.accepts_nested_attributes_for :children, :allow_destroy => false
       matcher.allow_destroy(true).matches?(parent).should be_false
       matcher.failure_message.should =~ /should allow destroy/
@@ -46,12 +41,12 @@ describe Shoulda::Matchers::ActiveRecord::AcceptNestedAttributesForMatcher do
   end
 
   context "limit" do
-    it "should accept a correct value" do
+    it "accepts a correct value" do
       Parent.accepts_nested_attributes_for :children, :limit => 3
       matcher.limit(3).matches?(parent).should be_true
     end
 
-    it "should reject a false value" do
+    it "rejects a false value" do
       Parent.accepts_nested_attributes_for :children, :limit => 3
       matcher.limit(2).matches?(parent).should be_false
       matcher.failure_message.should =~ /limit should be 2, got 3/
@@ -59,26 +54,41 @@ describe Shoulda::Matchers::ActiveRecord::AcceptNestedAttributesForMatcher do
   end
 
   context "update_only" do
-    it "should accept a valid truthy value" do
+    it "accepts a valid truthy value" do
       Parent.accepts_nested_attributes_for :children, :update_only => true
       matcher.update_only(true).matches?(parent).should be_true
     end
 
-    it "should accept a valid falsey value" do
+    it "accepts a valid falsey value" do
       Parent.accepts_nested_attributes_for :children, :update_only => false
       matcher.update_only(false).matches?(parent).should be_true
     end
 
-    it "should reject an invalid truthy value" do
+    it "rejects an invalid truthy value" do
       Parent.accepts_nested_attributes_for :children, :update_only => true
       matcher.update_only(false).matches?(parent).should be_false
       matcher.failure_message.should =~ /should not be update only/
     end
 
-    it "should reject an invalid falsey value" do
+    it "rejects an invalid falsey value" do
       Parent.accepts_nested_attributes_for :children, :update_only => false
       matcher.update_only(true).matches?(parent).should be_false
       matcher.failure_message.should =~ /should be update only/
+    end
+  end
+
+  def accepting_children(options)
+    define_model :child, :parent_id => :integer
+    define_model :parent do
+      has_many :children
+      accepts_nested_attributes_for :children, options
+    end
+  end
+
+  def rejecting
+    define_model :child, :parent_id => :integer
+    define_model :parent do
+      has_many :children
     end
   end
 end
